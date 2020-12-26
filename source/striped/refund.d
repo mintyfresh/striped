6f79f4fc588d7614bdf4634d7151a7eac1634c1b
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2020-12-26 15:27:21 -0500

    Add Stripe Refund object.

---
+++ source/striped/refund.d
@@ -0,0 +1,24 @@
+module striped.refund;
+
+import striped.balance_transaction;
+import striped.charge;
+import striped.object;
+
+struct StripeRefund
+{
+    mixin StripeObject!("refund");
+
+    mixin field!(int, "amount");
+    mixin field!(int, "created");
+    mixin field!(string, "currency");
+    mixin field!(string, "description");
+    mixin field!(string, "failureReason", "failure_reason");
+    mixin field!(JSONValue, "metadata");
+    mixin field!(string, "reason");
+    mixin field!(string, "recipientNumber", "recipient_number");
+    mixin field!(string, "status");
+
+    mixin expandable!(StripeBalanceTransaction, "balanceTransaction", "balance_transaction");
+    mixin expandable!(StripeCharge, "charge");
+    mixin expandable!(StripeBalanceTransaction, "failureBalanceTransaction", "failure_balance_transaction");
+}
